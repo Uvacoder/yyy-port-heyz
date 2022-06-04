7d76b22c92b9bda5a0a0459d76ec50c1a58f7546
Zaib <95955709+zaibcode@users.noreply.github.com>
2022-06-04 23:31:59 +0530

    Master-revamp of website

--- src/components/TwitterButton.tsx
+++ src/components/TwitterButton.tsx
@@ -1,15 +1,15 @@
 import { motion } from "framer-motion";
 
-export const GitHubButton = () => {
+export const TwitterButton = () => {
     return (
         <motion.a
-            href="https://github.com/heyzaib"
+            href="https://twitter.com/heyzaib"
             target="_blank"
             className="px-6 py-2 bg-slate-800 border-2 border-slate-700 text-white text-lg font-normal flex items-center justify-center rounded-md mt-7"
             whileHover={{ scale: 0.97 }}
             whileTap={{ scale: 0.9 }}
         >
-            GitHub
+            Twitter
         </motion.a>
     );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,28 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { GitHubButton } from "../components/GitHubButton";
+import { TwitterButton } from "../components/TwitterButton";
+import { motion } from "framer-motion";
 
 const Home: NextPage = () => {
     return (
         <>
             <Head>
-                <title>cnrad's next.js template</title>
+                <title>Zaib</title>             
             </Head>
-            <div className="w-full h-[100vh] flex flex-col items-center justify-center text-white text-3xl font-semibold">
-                cnrad&apos;s next.js template
-                <GitHubButton />
-            </div>
+            
+            <motion.div initial={{ opacity: 0, y: 40 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, scale: 0.95 }}
+                        transition={{ ease: "easeOut", duration: 0.6 }}>
+                <div className="w-full h-[100vh] flex flex-col items-center justify-center text-white text-5xl font-semibold">
+                    Hey, 👋 I am Zaib.
+                    <div className="flex flex-row space-x-4">
+                        <GitHubButton />
+                        <TwitterButton />
+                    </div>
+                </div>    
+            </motion.div>
         </>
     );
 };
