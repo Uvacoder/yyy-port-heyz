f3c265eec1db4afd0fd1e60f789592ae0ef8d723
Zaib <95955709+zaibcode@users.noreply.github.com>
2022-06-06 23:47:25 +0530

    bold text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
                         transition={{ ease: "easeOut", duration: 0.6 }}>
                 <div className="w-full h-[100vh] flex flex-col items-center justify-center text-center">
                     <h1 className="text-white font-semibold text-5xl selection:bg-white selection:text-black">Hey,👋 I am Zaib.</h1>
-                    <p className="text-white opacity-90 mt-5 selection:bg-white selection:text-black">I am a professional Motion Designer and an Avid Programmer.</p>
+                    <p className="text-white opacity-90 mt-5 selection:bg-white selection:text-black">I am a <b>Professional Motion Designer</b> and an Avid Programmer.</p>
                     <div className="flex flex-row space-x-4">
                         <GitHubButton />
                         <TwitterButton />
